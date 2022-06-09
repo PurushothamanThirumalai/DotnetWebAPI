d6e6f79acb176e12c4f3361e607fd1e7487af56f
Purushothaman <purushinbox@gmail.com>
2022-06-09 18:27:33 +0530

    First commit

---
+++ TestWebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestWebAPI.Model;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TestWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        // GET: api/<CustomerController>
+        [HttpGet]
+        public IEnumerable<Customer> Get()
+        {
+           List<Customer> _list=new List<Customer>();
+            _list.Add(new Customer() { Firstname = "Ram", Lastname = "Lakshman" });
+            _list.Add(new Customer() { Firstname = "Lava", Lastname = "Kusha" });
+            return _list;
+            //return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<CustomerController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<CustomerController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<CustomerController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<CustomerController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TestWebAPI/Model/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace TestWebAPI.Model
+{
+    public class Customer
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+
+    }
+}
